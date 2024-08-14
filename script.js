8afde3f1c58c764f47587586c1d97222db5c1467
Saurabh Kumar Vishwakarma <137218388+saurabhvish93@users.noreply.github.com>
2024-08-14 21:35:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	//  * create a frequency map; where we find all the frequency for each character
+const charFreq = {}; // charFreq["Name of Field"] 
+for(let char of str) {
+    charFreq[char] = (charFreq[char] || 0) + 1; // Language Specific || JS 
+}
+ 
+//  * Iterate over the string again and check if any character has frequency as 1. 
+//  *              If Yes; immediately return that particular character bcoz; it's was the first one
+for (let char of str) {
+    if(charFreq[char] === 1) {
+        return char
+    }
+}
+return null;
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
